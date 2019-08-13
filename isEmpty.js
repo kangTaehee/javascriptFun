99da6596e7570cdb60b398a58d09948bce016903
kangTaehee <ilovekth@nate.com>
2019-08-14 01:27:09 +0900

    for 문으로 변경

--- isEmpty.js
+++ isEmpty.js
@@ -1,13 +1,13 @@
 // null, undefined 값 '' 처리
 function isEmpty(argument) {
     if (argument == null || argument == undefined) {return ''}
-    $.each(argument, function(key, value){
-        if( value == null || value == undefined ){
-            argument[key] = ''
-        }
-        if (typeof argument[key] == 'object') {
-            isEmpty(argument[key])
-        }
-    });
+	for(var key in argument){
+		if (argument[key] == null || argument[key] == undefined ) {
+			argument[key] = ''
+		}
+		if (typeof argument[key] == 'object') {
+			isEmpty(argument[key])
+		}
+	}
     return argument
 }
