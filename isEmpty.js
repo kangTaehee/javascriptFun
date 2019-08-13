e5dd734e45c9dd3af35899c8fb1b84955901f3ab
kangTaehee <ilovekth@nate.com>
2019-08-14 01:03:43 +0900

    단일 속성 처리 추가

--- isEmpty.js
+++ isEmpty.js
@@ -1,5 +1,6 @@
 // null, undefined 값 '' 처리
 function isEmpty(argument) {
+    if (argument == null || argument == undefined) {return ''}
     $.each(argument, function(key, value){
         if( value == null || value == undefined ){
             argument[key] = ''
